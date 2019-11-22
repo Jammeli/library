08eb5b55fcdd6f5d429ba0d2960bf4f42d429843
Marwen - JAMMELI <marwen.jammeli@gmail.com>
2019-11-22 11:35:17 +0100

    Add files via upload

---
+++ src/FilmothequeBundle/Entity/User.php
@@ -0,0 +1,118 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace FilmothequeBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Nom", type="string", length=255 , nullable=true)
+     */
+    protected $Nom;
+   /**
+     * @var string
+     *
+     * @ORM\Column(name="Prenom", type="string", length=255 , nullable=true)
+     */
+    protected $Prenom;
+   /**
+     * @var string
+     *
+     * @ORM\Column(name="Adresse", type="string", length=255 ,nullable=true )
+     */
+    protected $Adresse;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->Nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->Nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->Prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->Prenom;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return User
+     */
+    public function setAdresse($adresse)
+    {
+        $this->Adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->Adresse;
+    }
+}
---
+++ src/FilmothequeBundle/Entity/client.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace FilmothequeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * client
+ *
+ * @ORM\Table(name="client")
+ * @ORM\Entity(repositoryClass="FilmothequeBundle\Repository\clientRepository")
+ */
+class client
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numtel", type="string", length=255)
+     */
+    private $numtel;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return client
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return client
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return client
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set numtel
+     *
+     * @param string $numtel
+     *
+     * @return client
+     */
+    public function setNumtel($numtel)
+    {
+        $this->numtel = $numtel;
+
+        return $this;
+    }
+
+    /**
+     * Get numtel
+     *
+     * @return string
+     */
+    public function getNumtel()
+    {
+        return $this->numtel;
+    }
+}
+
